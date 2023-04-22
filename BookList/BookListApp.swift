a84070bfcb56413b7a0a2e09ddfd87ce80f38736
Tom Montgomery <montgomery3681@gmail.com>
2023-04-21 19:13:14 -0600

    Initial Commit

---
+++ BookList/BookListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookListApp.swift
+//  BookList
+//
+//  Created by tom montgomery on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BookListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
